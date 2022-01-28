e81dbb7fe9034eb204611f842dee58627b896423
hvalmer <hvalmer@gmail.com>
2022-01-27 23:48:13 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,29 @@
-import Pagination from 'components/Pagination/index';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import MovieCard from 'components/MovieCard/index';
+import Pagination from 'components/Pagination/index';
+import { BASE_URL } from 'utils/requests';
+import { MoviePage } from 'types/movie';
 
 function Listing() {
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+  //executa a função useEffect, somente quando carregar o componente
+  //executando detro de um hooks, que amarra dentro do ciclo de vida do componente
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    .then(response => {
+      const data = response.data as MoviePage;
+      console.log(data);  
+      setPageNumber(data.number);
+     });
+  
+  }, []);
+
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
 
       <div className='container'>
