599969e56019a2ec0eb698819c29e1f8ce3f9d23
hvalmer <hvalmer@gmail.com>
2022-02-01 11:21:08 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
+import {  validateEmail } from 'utils/validate';
 import './styles.css';
 
 type Props = {
@@ -10,6 +11,9 @@ type Props = {
 }
 
 function FormCard( { movieId } : Props ) {
+
+  //comando para redicionamento de rota
+  const navigate = useNavigate();
 
   //lógica pra buscar do backend esse filme, a partir do id do filme
   const[movie, setMovie] = useState<Movie>();
@@ -24,12 +28,42 @@ function FormCard( { movieId } : Props ) {
   //Não colocando como dependência, a requisição axio, ficaria sendo feita várias vezes
   //e essa requisição do id só tem que vir uma vez de cada 
   
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
+
+    event.preventDefault();
+
+    const email = (event.target as any).email.value;
+    const score = (event.target as any).score.value;
+
+    //se por acaso o email não for válido, não faz nada na hora de validar
+    if(!validateEmail(email)){
+        return;
+    }
+
+    //configuração de requisição do axios
+    const config: AxiosRequestConfig = {
+      baseURL: BASE_URL,
+      method: 'PUT',
+      url: '/scores',
+      data: {
+        email: email,
+        movieId: movieId,
+        score: score
+      }
+    }
+
+    //requisição do PUT
+    axios(config).then(response =>{
+        navigate("/");//volta pra página inicial
+    });
+  }
+
   return (
     <div className="dsmovie-form-container">
       <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.title} />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie?.title}</h3>
-        <form className="dsmovie-form">
+        <form className="dsmovie-form" onSubmit={handleSubmit}>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="email">Informe seu email</label>
             <input type="email" className="form-control" id="email" />
